3c6a8f1ec7f0557ce483fb5b05a6bfcfcb834e15
Apowalowski <acpowalowski@gmail.com>
2021-04-02 12:44:52 -0400

    operationsexercise

---
+++ operation exercise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace operationsexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            string result = $"{a} / {b}";
+            if (a==17 && b==4)
+
+            Console.WriteLine(result);
+            Console.WriteLine(remainder);
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
